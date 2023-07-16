a814b28590975b5bdb03599e3aa76e8ef241cd7e
Nalinaka <nalinaka@gmail.com>
2023-07-15 21:34:24 -0700

    completed assignment

---
+++ src/Components/BackImage.js
@@ -0,0 +1,11 @@
+
+
+function BackgroundImage () {
+    return (
+       <div>
+        <h1>Order groceries for delivery or pick up today</h1>
+        <p>Whatever you want from local stores, brought right to your door.</p>
+        < img src = "src/Components/Boostrap Replica background image.webp" alt = "BackgroundImage" ></img>
+       </div> 
+    )
+}
